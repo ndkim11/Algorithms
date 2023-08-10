dd35bccdb7c21fd5f5938d66e2bc045ac46c137a
김상윤 <68493794+ndkim11@users.noreply.github.com>
2023-08-10 21:15:17 +0900

    [Bronze I] Title: 암호 키, Time: 1272 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,11 @@
+cases = int(input())
+
+for _ in range(cases):
+    num = int(input())
+    for i in range(2, 1000001):
+        if(num % i == 0):
+            print("NO")
+            break
+
+        if(i == 1000000):
+            print("YES")
